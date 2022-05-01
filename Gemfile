99a1d025d3647f7ebc783a26af789fe44dd3a263
IkeCairns <zxh03501@nifty.com>
2022-05-01 22:05:22 +1000

    Finish working and refactored palindrome method

--- Gemfile
+++ Gemfile
@@ -3,10 +3,8 @@
 source "https://rubygems.org"
 
 # Specify your gem's dependencies in ikuya_palindrome.gemspec
-gemspec
 
 gem "rake", "~> 13.0"
-
 gem "minitest", "~> 5.0"
-
 gem "standard", "~> 1.3"
+gem 'minitest-reporters', '1.2.0'
--- lib/ikuya_palindrome.rb
+++ lib/ikuya_palindrome.rb
@@ -1,8 +1,16 @@
-# frozen_string_literal: true
+require "ikuya_palindrome/version"
 
-require_relative "ikuya_palindrome/version"
+class String
 
-module IkuyaPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+  #Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      scan(/[a-z]/i).join.downcase
+    end
 end
