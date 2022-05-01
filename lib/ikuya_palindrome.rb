d656c09ef31acf0f6a3f9bac356a8d274dbcf0d5
IkeCairns <zxh03501@nifty.com>
2022-05-01 19:34:14 +1000

    Initialise repository

---
+++ lib/ikuya_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ikuya_palindrome/version"
+
+module IkuyaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ikuya_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module IkuyaPalindrome
+  VERSION = "0.1.0"
+end
